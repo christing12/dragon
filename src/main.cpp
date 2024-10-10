b172fce74628f6ccc2a08246c31d47880bac2a56
Chris Ting <chris.ting@unity3d.com>
2024-10-10 13:41:42 -0700

    initialized vkinstance successfully with volk and vkbootstrap

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <stdio.h>
 
+#define VOLK_IMPLEMENTATION
+#include <volk.h>
 
-
+#include "VkBootstrap.h"
 
 int main() {
+    VkResult result = volkInitialize();
+    //VkInstance instance;
+    //volkLoadInstance(instance);
+
+    vkb::InstanceBuilder builder;
+    auto instanceBuilderResult = builder.set_app_name("Vulkan Dragon")
+        .request_validation_layers()
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
+
+    vkb::Instance vkbInstance = instanceBuilderResult.value();
+    VkInstance vkInstance = vkbInstance.instance;
+    VkDebugUtilsMessengerEXT vkDebugMessenger = vkbInstance.debug_messenger;
+
+    volkLoadInstance(vkInstance);
+
     printf("HELLO WORLD\n");
     return 0;
 }
